7eb5454d4dc6383535a393137c634b8b43059972
Dalton Patterson <dpatt@fullsail.edu>
2015-09-23 14:36:51 -0600

    Update accordion.js
    up

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -13,7 +13,7 @@ $(document).ready(function($) {
 		//Expand or collapse this panel
 		$(this).next().slideToggle('fast');
 		//Hide the other panels
-		$(".accordion-content").not($(this).next()).slideDown('slow');
+		$(".accordion-content").not($(this).next()).slideUp('slow');
 	});
 	
 	function sticky_relocate() {
