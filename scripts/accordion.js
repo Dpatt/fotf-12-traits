1d15878802792c55544c8be3642181078deaeb2d
Dalton Patterson <dpatt@fullsail.edu>
2015-09-22 18:03:54 -0600

    First Page commit

---
+++ scripts/accordion.js
@@ -0,0 +1,33 @@
+$(document).ready(function($) {
+	// Open Content
+	$('.accordion-item').find('.accordion-header').click(function() {
+		//Expand or collapse this panel
+		$(this).next().slideToggle('fast');
+		
+		//Hide the other panels
+		$(".accordion-content").not($(this).next()).slideUp('fast');
+	});
+	
+	// Close Content
+	$('.accordion-item').find('.close-btn').click(function() {
+		//Expand or collapse this panel
+		$(this).next().slideToggle('fast');
+		//Hide the other panels
+		$(".accordion-content").not($(this).next()).slideUp('slow');
+	});
+	
+	function sticky_relocate() {
+      var window_top = $(window).scrollTop();
+      var div_top = $('#sticky-anchor').offset().top;
+      if (window_top > div_top)
+        $('#inner-report').addClass('sticky')
+      else
+        $('#inner-report').removeClass('sticky');
+      }
+     
+     $(function() {
+      $(window).scroll(sticky_relocate);
+      sticky_relocate();
+      });
+     
+});
