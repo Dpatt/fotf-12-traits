46d2e7111ea7a8ddd6a10028107f07acf31178c2
Dalton Patterson <dpatt@fullsail.edu>
2015-09-23 14:32:37 -0600

    Update accordion.js
    slide change

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -13,7 +13,7 @@ $(document).ready(function($) {
 		//Expand or collapse this panel
 		$(this).next().slideToggle('fast');
 		//Hide the other panels
-		$(".accordion-content").not($(this).next()).slideUp('slow');
+		$(".accordion-content").not($(this).next()).slidedown('slow');
 	});
 	
 	function sticky_relocate() {
